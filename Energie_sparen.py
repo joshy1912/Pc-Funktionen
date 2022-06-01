00c1bb5a80ebb021a0be2ae8fbbaae74c2b030b9
joshy1912 <99416967+joshy1912@users.noreply.github.com>
2022-06-01 20:03:48 +0200

    Pc Funktionen
    Das sind Programme mit denen man auf Windows Neustarten, Ausschalten oder Engergie Sparen machen kann

---
+++ Energie_sparen.py
@@ -0,0 +1,7 @@
+import pyautogui as py
+py.press ('win')
+py.sleep (1)
+py.moveTo (x=25, y=771)
+py.click ()
+py.moveTo (x=125, y=631)
+py.click
---
+++ NeuStarten.py
@@ -0,0 +1,7 @@
+import pyautogui as py
+py.press ('win')
+py.sleep (1)
+py.moveTo (x=25, y=771)
+py.click ()
+py.moveTo (x=25, y=702)
+py.click ()
